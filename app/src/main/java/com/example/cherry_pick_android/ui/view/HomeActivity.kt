5a56bd349a974efec3aed7f17988ddc8abc15ae1
arinming <jyandysys1@naver.com>
2023-07-20 14:49:11 +0900

    [Refactor] : HomeNews를 Activity에서 Fragment로 변경, HomeActivity 추가

--- app/src/main/java/com/example/cherry_pick_android/ui/view/HomeActivity.kt
+++ app/src/main/java/com/example/cherry_pick_android/ui/view/HomeActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.cherry_pick_android.R
 
-class HomeNewsActivity: AppCompatActivity() {
+class HomeActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_news)
