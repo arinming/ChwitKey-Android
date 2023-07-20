d89f147880503b00828d4bbb25a08c5b6fef63bd
arinming <jyandysys1@naver.com>
2023-07-20 14:37:31 +0900

    [Feat] : HomeNews 와이어프레임 기본 틀 구현

---
+++ app/src/main/java/com/example/cherry_pick_android/ui/view/HomeNewsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cherry_pick_android.ui.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cherry_pick_android.R
+
+class HomeNewsActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_news)
+    }
+}
