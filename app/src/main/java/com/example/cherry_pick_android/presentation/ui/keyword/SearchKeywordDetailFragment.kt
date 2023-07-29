e1f081dfc795b8415a5a93b9a40b36eaf56078d5
JunYeong0314 <kkmtk00314@naver.com>
2023-07-29 18:36:38 +0900

    [Feat] 최초 키워드 등록 로직 구현

---
+++ app/src/main/java/com/example/cherry_pick_android/presentation/ui/keyword/SearchKeywordDetailFragment.kt
@@ -0,0 +1,25 @@
+package com.example.cherry_pick_android.presentation.ui.keyword
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.cherry_pick_android.databinding.FragmentSearchKeywordDetailBinding
+
+class SearchKeywordDetailFragment: Fragment() {
+    private val binding: FragmentSearchKeywordDetailBinding by lazy {
+        FragmentSearchKeywordDetailBinding.inflate(layoutInflater)
+    }
+    companion object{
+        const val TAG = "searchKeywordDetailFragment"
+        fun newInstance(): SearchKeywordDetailFragment = SearchKeywordDetailFragment()
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return binding.root
+    }
+}
