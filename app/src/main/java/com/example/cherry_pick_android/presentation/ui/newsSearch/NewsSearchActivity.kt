7bb645f006cacc2a0e3ba1979380382ff2cc94e2
arinming <jyandysys1@naver.com>
2023-07-30 14:12:56 +0900

    [Feat] : 뉴스 검색 액티비티 - 키워드 리사이클러뷰 구현

--- app/src/main/java/com/example/cherry_pick_android/presentation/ui/newsSearch/NewsSearchActivity.kt
+++ app/src/main/java/com/example/cherry_pick_android/presentation/ui/newsSearch/NewsSearchActivity.kt
@@ -3,17 +3,30 @@ package com.example.cherry_pick_android.presentation.ui.newsSearch
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.cherry_pick_android.databinding.ActivityNewsSearchBinding
+import com.example.cherry_pick_android.presentation.adapter.KeywordAdapter
 import com.example.cherry_pick_android.presentation.ui.home.HomeActivity
+import com.example.cherry_pick_android.presentation.ui.keyword.Keyword
 import com.example.cherry_pick_android.presentation.ui.searchList.SearchListActivity
 
 class NewsSearchActivity: AppCompatActivity() {
     private lateinit var binding: ActivityNewsSearchBinding
+
+    private val keywords = listOf(
+        Keyword("2차전지"), Keyword("IT"), Keyword("철강"), Keyword("정유"),
+        Keyword("석유"), Keyword("반도체"), Keyword("디스플레이"), Keyword("휴대폰"),
+        Keyword("반도체"), Keyword("해운"), Keyword("F&B"), Keyword("건설"),
+        Keyword("소매유통"), Keyword("건설"), Keyword("철강"), Keyword("정유")
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNewsSearchBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        initView()
 
         goToBack()
         goToSearchList()
@@ -32,5 +45,10 @@ class NewsSearchActivity: AppCompatActivity() {
             startActivity(Intent(this, SearchListActivity::class.java))
             finish()
         }
+    }
+
+    fun initView(){
+        binding.rvSearchNewsList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        binding.rvSearchNewsList.adapter = KeywordAdapter(keywords)
     }
 }
