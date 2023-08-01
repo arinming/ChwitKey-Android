8c8f70e641479e45c2931f4ea40d507fa7300859
arinming <jyandysys1@naver.com>
2023-08-01 18:11:34 +0900

    [Feat] : 뉴스 페이지 레이아웃 구현 (#46)

--- app/src/main/java/com/example/cherry_pick_android/presentation/ui/article/ArticleActivity.kt
+++ app/src/main/java/com/example/cherry_pick_android/presentation/ui/article/ArticleActivity.kt
@@ -1,7 +1,15 @@
 package com.example.cherry_pick_android.presentation.ui.article
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.cherry_pick_android.databinding.ActivityArticleBinding
 
 class ArticleActivity: AppCompatActivity() {
-
+    private lateinit var binding: ActivityArticleBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityArticleBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
 }
