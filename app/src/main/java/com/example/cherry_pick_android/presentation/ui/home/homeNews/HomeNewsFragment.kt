c6598fd020d2ce20d5ea9ce3b4270ef878faf6ce
arinming <jyandysys1@naver.com>
2023-08-20 00:33:09 +0900

    [Feat] : 선택된 직군, 정렬순으로 기사 불러오기 (#121)

--- app/src/main/java/com/example/cherry_pick_android/presentation/ui/home/homeNews/HomeNewsFragment.kt
+++ app/src/main/java/com/example/cherry_pick_android/presentation/ui/home/homeNews/HomeNewsFragment.kt
@@ -1,6 +1,5 @@
 package com.example.cherry_pick_android.presentation.ui.home.homeNews
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -20,7 +19,6 @@ import com.example.cherry_pick_android.data.remote.service.user.UserInfoService
 import com.example.cherry_pick_android.databinding.FragmentHomeNewsBinding
 import com.example.cherry_pick_android.domain.repository.UserDataRepository
 import com.example.cherry_pick_android.presentation.adapter.IndustryAdapter
-import com.example.cherry_pick_android.presentation.adapter.KeywordListAdapter
 import com.example.cherry_pick_android.presentation.adapter.NewsRecyclerViewAdapter
 import com.example.cherry_pick_android.presentation.ui.keyword.AdapterInteractionListener
 import com.example.cherry_pick_android.presentation.ui.newsSearch.NewsSearchActivity
@@ -35,17 +33,19 @@ class HomeNewsFragment : Fragment(R.layout.fragment_home_news), AdapterInteracti
     private var _binding: FragmentHomeNewsBinding? = null
     private val binding get() = _binding!!
 
-    private var industryInit : String = ""
+    private var industryInit: String = ""
+    private var isMenuOpen = false
 
     @Inject
     lateinit var articleService: ArticleSearchCommendService
+
     @Inject
     lateinit var userInfoService: UserInfoService
+
     @Inject
     lateinit var userDataRepository: UserDataRepository
 
     private lateinit var selectedIndustry: String
-    private lateinit var industryAdapter: IndustryAdapter
 
     companion object {
         const val TAG = "HomeNewsFragmnet"
@@ -65,7 +65,6 @@ class HomeNewsFragment : Fragment(R.layout.fragment_home_news), AdapterInteracti
         super.onViewCreated(view, savedInstanceState)
 
         industryLoad()
-        getArticleList()
         goToNewsSearch()
 
 
@@ -74,9 +73,9 @@ class HomeNewsFragment : Fragment(R.layout.fragment_home_news), AdapterInteracti
             userDataRepository.getUserData()
         }
 
-
-
-        binding.ibtnSortingMenu.setOnClickListener { showSortingMenu(it) }
+        binding.ibtnSortingMenu.setOnClickListener {
+            showSortingMenu(it)
+        }
     }
 
     override fun onDestroyView() {
@@ -84,27 +83,31 @@ class HomeNewsFragment : Fragment(R.layout.fragment_home_news), AdapterInteracti
         _binding = null
     }
 
-    private fun getArticleList() {
+    private fun getArticleList(sort: String) {
         // API 통신
         lifecycleScope.launch {
             withContext(Dispatchers.Main) {
-                var sort = binding.tvSorting.text
-
-                when (sort) {
-                    "인기순" -> sort = "like"
-                    "오름차순" -> sort = "asc"
-                    "내림차순" -> sort = "desc"
-                }
-
-                val response = articleService.getArticleCommend(industryInit, sort.toString(), Pageable)
-                Log.d("요청 직군", industryInit)
+                val response = articleService.getArticleCommend(
+                    cond = industryInit,
+                    sortType = sort,
+                    pageable = Pageable
+                )
+                Log.d("직군 정렬", "$industryInit, $sort")
 
                 val statusCode = response.body()?.statusCode
                 if (statusCode == 200) {
                     onIndustryButtonClick(industryInit)
+
                     val articleItems = response.body()?.data?.content?.map { content ->
-                        val imageUrl = if (content.articlePhoto.isNotEmpty()) content.articlePhoto[0].articleImgUrl else "" // 기사 사진이 없으면 빈 문자열로 처리
-                        ArticleItem(content.title, content.publisher, content.uploadedAt, imageUrl, content.articleId)
+                        val imageUrl =
+                            if (content.articlePhoto.isNotEmpty()) content.articlePhoto[0].articleImgUrl else "" // 기사 사진이 없으면 빈 문자열로 처리
+                        ArticleItem(
+                            content.title,
+                            content.publisher,
+                            content.uploadedAt,
+                            imageUrl,
+                            content.articleId
+                        )
                     }
                     binding.rvNewsList.adapter = NewsRecyclerViewAdapter(articleItems)
                 } else {
@@ -114,9 +117,7 @@ class HomeNewsFragment : Fragment(R.layout.fragment_home_news), AdapterInteracti
         }
     }
 
-    private fun initView() {
-        industryInit
-    }
+
 
     // 검색창 누르면 NewsSearch 액티비티로 이동
     private fun goToNewsSearch() {
@@ -129,7 +130,6 @@ class HomeNewsFragment : Fragment(R.layout.fragment_home_news), AdapterInteracti
     }
 
 
-
     // 메뉴
     private fun showSortingMenu(view: View) {
         val popupMenu = PopupMenu(requireContext(), view)
@@ -138,29 +138,48 @@ class HomeNewsFragment : Fragment(R.layout.fragment_home_news), AdapterInteracti
         // 메뉴 아이템 클릭 처리
         popupMenu.setOnMenuItemClickListener { item ->
             when (item.itemId) {
-                R.id.menu_sort_asc -> binding.tvSorting.text = getString(R.string.sort_article_asc)
-                R.id.menu_sort_desc -> binding.tvSorting.text = getString(R.string.sort_article_desc)
-                R.id.menu_sort_like -> binding.tvSorting.text = getString(R.string.sort_article_like)
+                R.id.menu_sort_asc -> {
+                    binding.tvSorting.text = getString(R.string.sort_article_asc)
+                    getArticleList("asc")
+                }
+                R.id.menu_sort_desc -> {
+                    binding.tvSorting.text = getString(R.string.sort_article_desc)
+                    getArticleList("desc")
+                }
+                R.id.menu_sort_like -> {
+                    binding.tvSorting.text = getString(R.string.sort_article_like)
+                    getArticleList("like")
+                }
             }
-            getArticleList()
-
             true
         }
         popupMenu.show()
     }
 
-    private fun mapperToJob(value: String): String{
-        return when(value){
-            "steel" -> "철강" "Petroleum/Chemical" -> "석유·화학" "oilrefining" -> "정유" "secondarybattery" -> "2차 전지"
-            "Semiconductor" -> "반도체" "Display" -> "디스플레이" "Mobile" -> "휴대폰" "It" -> "IT"
-            "car" -> "자동차" "Shipbuilding" -> "조선" "Shipping" -> "해운" "FnB" -> "F&B"
-            "RetailDistribution" -> "소매유통" "Construction" -> "건설" "HotelTravel" -> "호텔·여행·항공" "FiberClothing" -> "섬유·의류"
+    private fun mapperToJob(value: String): String {
+        return when (value) {
+            "steel" -> "철강"
+            "Petroleum/Chemical" -> "석유·화학"
+            "oilrefining" -> "정유"
+            "secondarybattery" -> "2차 전지"
+            "Semiconductor" -> "반도체"
+            "Display" -> "디스플레이"
+            "Mobile" -> "휴대폰"
+            "It" -> "IT"
+            "car" -> "자동차"
+            "Shipbuilding" -> "조선"
+            "Shipping" -> "해운"
+            "FnB" -> "F&B"
+            "RetailDistribution" -> "소매유통"
+            "Construction" -> "건설"
+            "HotelTravel" -> "호텔·여행·항공"
+            "FiberClothing" -> "섬유·의류"
             else -> ""
         }
     }
 
 
-    private fun industryLoad(){
+    private fun industryLoad() {
         lifecycleScope.launch {
             val response = userInfoService.getUserInfo().body()
             val statusCode = response?.statusCode
@@ -183,37 +202,55 @@ class HomeNewsFragment : Fragment(R.layout.fragment_home_news), AdapterInteracti
             onIndustryButtonClick(industry1)
 
 
-            withContext(Dispatchers.Main){
-                if(statusCode == 200){
+            withContext(Dispatchers.Main) {
+                if (statusCode == 200) {
                     Log.d("현재 직군", industryInit)
+                    loadArticlesByIndustry(industry1)
 
                     binding.rvIndustry.layoutManager =
                         LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
                     binding.rvIndustry.adapter = IndustryAdapter(industries, this@HomeNewsFragment)
-                    Log.d("직군","$industry1, $industry2, $industry3")
-                } else{
+                    Log.d("직군", "$industry1, $industry2, $industry3")
+                } else {
                     Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show()
                 }
             }
         }
+
     }
 
     // 버튼 클릭 시 호출되는 함수
-    fun onIndustryButtonClick(industry: String) {
+    private fun onIndustryButtonClick(industry: String) {
         industryInit = industry // 클릭한 버튼의 텍스트를 저장
         Log.d("직군 함수 호출", industryInit) // 클릭한 버튼의 텍스트 로그로 출력
     }
 
     // 버튼 클릭시 뉴스 리스트 갱신
-    private fun loadArticlesByKeyword(keyword: String) {
+    private fun loadArticlesByIndustry(keyword: String) {
         lifecycleScope.launch {
             withContext(Dispatchers.Main) {
-                val response = articleService.getArticleCommend(sortType = "desc", cond = keyword, pageable = Pageable)
+                val response = articleService.getArticleCommend(
+                    sortType = when (binding.tvSorting.text) {
+                        "인기순" -> "like"
+                        "오름차순" -> "asc"
+                        "내림차순" -> "desc"
+                        else -> ""
+                    },
+                    cond = keyword,
+                    pageable = Pageable
+                )
 
                 // 기사를 가져온 후에 아래와 같이 어댑터에 기사 리스트를 전달하여 갱신
                 val articleItems = response.body()?.data?.content?.map { content ->
-                    val imageUrl = if (content.articlePhoto.isNotEmpty()) content.articlePhoto[0].articleImgUrl else ""
-                    ArticleItem(content.title, content.publisher, content.uploadedAt, imageUrl, content.articleId)
+                    val imageUrl =
+                        if (content.articlePhoto.isNotEmpty()) content.articlePhoto[0].articleImgUrl else ""
+                    ArticleItem(
+                        content.title,
+                        content.publisher,
+                        content.uploadedAt,
+                        imageUrl,
+                        content.articleId
+                    )
                 }
                 binding.rvNewsList.adapter = NewsRecyclerViewAdapter(articleItems)
             }
@@ -222,6 +259,7 @@ class HomeNewsFragment : Fragment(R.layout.fragment_home_news), AdapterInteracti
 
     override fun onButtonSelected(button: String) {
         selectedIndustry = button
-        loadArticlesByKeyword(button)
+        industryInit = button
+        loadArticlesByIndustry(button)
     }
 }
