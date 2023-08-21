8bce617b44c6454e5b3f58546ebc5fac5b5b231c
arinming <jyandysys1@naver.com>
2023-08-21 21:00:56 +0900

    [Fix] : 스크랩 Adapter 변경 (#135)

--- app/src/main/java/com/example/cherry_pick_android/presentation/adapter/ScrapAdapter.kt
+++ app/src/main/java/com/example/cherry_pick_android/presentation/adapter/ScrapAdapter.kt
@@ -6,13 +6,13 @@ import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.cherry_pick_android.data.remote.response.article.ScrapData
+import com.example.cherry_pick_android.data.remote.response.article.ArticleScrapResponse.Data
 import com.example.cherry_pick_android.databinding.ItemScrapNewsBinding
 import com.example.cherry_pick_android.presentation.ui.article.ArticleActivity
 import com.example.cherry_pick_android.presentation.ui.scrap.DeleteScrapListener
 import com.google.android.material.snackbar.Snackbar
 
-class ScrapAdapter(private val listener: DeleteScrapListener, private val scrapNewsData: MutableList<ScrapData?>) :
+class ScrapAdapter(private val listener: DeleteScrapListener, private val scrapNewsData: MutableList<Data.Content>?) :
     RecyclerView.Adapter<ScrapAdapter.ViewHolder>() {
 
     // 뷰 유형에 대한 참조 클래스
@@ -49,7 +49,8 @@ class ScrapAdapter(private val listener: DeleteScrapListener, private val scrapN
 
     // 호출한 내용으로 bind
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        val scrapData = scrapNewsData[position]
+        val scrapData = scrapNewsData?.get(position)
+
         viewHolder.title.text = scrapData?.title
         viewHolder.company.text = scrapData?.publisher
         viewHolder.time.text = scrapData?.uploadedAt
@@ -62,14 +63,20 @@ class ScrapAdapter(private val listener: DeleteScrapListener, private val scrapN
         // 스크랩 버튼 클릭 이벤트 설정
         viewHolder.binding.ibtnScrap.setOnClickListener {
             listener.onDelete(viewHolder.id ?: 0)
-            scrapNewsData.removeAt(position) // 아이템 삭제
+            scrapNewsData?.removeAt(position) // 아이템 삭제
             Snackbar.make(it, "스크랩 목록에서 삭제했어요!", Snackbar.LENGTH_SHORT).show()
             notifyItemRemoved(position) // 삭제된 아이템 위치 업데이트
-            notifyItemRangeChanged(position, scrapNewsData.size)  // 아이템 사이즈 업데이트
+            scrapNewsData?.let { it1 -> notifyItemRangeChanged(position, it1.size) }  // 아이템 사이즈 업데이트
         }
     }
 
     // 데이터 크기 반환
-    override fun getItemCount() = scrapNewsData.size
-
+// 데이터 크기 반환
+    override fun getItemCount() : Int {
+        return if (scrapNewsData == null) {
+            0
+        } else {
+            scrapNewsData?.size!!
+        }
+    }
 }
